d81ee44cc08c705e8bed10728a6c272720a5abc9
Eduardo Santiago <eduardoc.santiago@hotmail.com>
2023-05-04 09:35:46 -0300

    a

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(primeiro, segundo):
+    soma = primeiro + segundo
+    return soma
+
+def subtracao(primeiro, segundo):
+    subtracao = primeiro - segundo
+    return subtracao
+
+def multiplicacao(primeiro, segundo):
+    multiplicacao = primeiro * segundo
+    return multiplicacao
+
+def divisao(primeiro, segundo):
+    divisao = primeiro / segundo
+    return divisao
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    # dados o numero 2 e o numero 5
+    primeiro = 2
+    segundo = 5
+
+    # calcula a soma
+    output = methods.soma(primeiro, segundo)
+    
+    # entao a soma deve ser 7
+    assert output == 7
+
+def test_subtracao():
+    # dados o numero 2 e o numero 5
+    primeiro = 2
+    segundo = 5
+
+    # calcula a subtracao
+    output = methods.subtracao(primeiro, segundo)
+    
+    # entao a subtracao deve ser 7
+    assert output == -3
+ 
+def test_multiplicacao():
+    # dados o numero 2 e o numero 5
+    primeiro = 2
+    segundo = 5
+
+    # calcula a multiplicacao
+    output = methods.multiplicacao(primeiro, segundo)
+    
+    # entao a multiplicacao deve ser 7
+    assert output == 10
+ 
+def test_divisao():
+    # dados o numero 2 e o numero 5
+    primeiro = 2
+    segundo = 5
+
+    # calcula a divisao
+    output = methods.divisao(primeiro, segundo)
+    
+    # entao a divisao deve ser 7
+    assert output == 0.4
